2ced37d858bc7e679ae0c9c598b9f548d396b957
paviudes <pavithran@xanadu.ai>
2026-03-11 20:51:02 -0400

    correlation strengths removed

--- src/command_line.jl
+++ src/command_line.jl
@@ -225,7 +225,7 @@ function parse_command_line_args_NN(;prefix::String="./../data")::Dict{String, A
 	# Ensure that the `./../data/codename` has all the required files.
 	if isdir("$(prefix)/$(args_dict["codename"])")
         # Check for required files
-		required_files = ["HX.txt", "LX.txt", "connectivity_matrix.txt", "correlation_strengths.txt", args_dict["train"], args_dict["test"]]
+		required_files = ["HX.txt", "LX.txt", "connectivity_matrix.txt", args_dict["train"], args_dict["test"]]
 		for file in required_files
 			if !isfile("$(prefix)/$(args_dict["codename"])/$(file)")
 				throw(error("The required file '$(file)' is missing in the directory '$(prefix)/$(args_dict["codename"])'. Please add the file and try again."))
